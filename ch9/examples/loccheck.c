78c951add72de76ff1fcd780b97c90ac4e920575
altronrain <altron24121992@gmail.com>
2026-03-05 23:16:11 +0300

    add examples ch.9

--- ch9/examples/loccheck.c
+++ ch9/examples/loccheck.c
@@ -7,8 +7,8 @@ int main (void)
 {
 	int pooh = 2, bah = 5;		/* локальные для main() */
 
-	printf ("Внутри main() pooh = %d\t&pooh = %p\n", pooh, &pooh);
-	printf ("Внутри main() bah  = %d\t&bah  = %p\n", bah, &bah);
+	printf ("Внутри main() pooh = %d\t\t&pooh = %p\n", pooh, &pooh);
+	printf ("Внутри main() bah  = %d\t\t&bah  = %p\n", bah, &bah);
 	mikado (pooh);
 
 	return 0;
@@ -18,14 +18,14 @@ void mikado (int bah)			/* переменная bah будет иметь фак
 {
 	int pooh = 10;
 
-	printf ("Внутри main() pooh = %d\t&pooh = %p\n", pooh, &pooh);
-	printf ("Внутри main() bah  = %d\t&bah  = %p\n", bah, &bah);
+	printf ("Внутри mikado() pooh = %d\t&pooh = %p\n", pooh, &pooh);
+	printf ("Внутри mikado() bah  = %d\t&bah  = %p\n", bah, &bah);
 }
 
 /*
 $ ./loccheck 
-Внутри main() pooh = 2  &pooh = 0x7ffd213105ac
-Внутри main() bah  = 5  &bah  = 0x7ffd213105a8
-Внутри main() pooh = 10 &pooh = 0x7ffd2131058c
-Внутри main() bah  = 2  &bah  = 0x7ffd2131057c
+Внутри main() pooh = 2          &pooh = 0x7ffe3dedb78c
+Внутри main() bah  = 5          &bah  = 0x7ffe3dedb788
+Внутри mikado() pooh = 10       &pooh = 0x7ffe3dedb76c
+Внутри mikado() bah  = 2        &bah  = 0x7ffe3dedb75c
 */
